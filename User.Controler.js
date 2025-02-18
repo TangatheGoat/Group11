06451492ebaa24b3c00dd2ea56e89d5accfbd388
James Thorpe <93221003+degas123@users.noreply.github.com>
2025-02-18 12:53:26 +0000

    Update User.Controler.js
    updated text

--- User.Controler.js
+++ User.Controler.js
@@ -6,23 +6,20 @@ const charicters = new RegExp("^[a-zA-Z]+$")
 
 // creates the users account. auther @James 
 const create_account = (req, res) => {
-
     const schema = Joi.object({
         first_name: Joi.string().min(1).alphanum().pattern(charicters).required(),
         last_name: Joi.string().min(1).alphanum().pattern(charicters).required(),
         email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
         password: Joi.string().regex(password).required()
-
     })
     const { error } = schema.validate(req.body);
-
     if (error) return res.status(400).send(
         { "error_message": error.details[0].message }
     );
       Users.create_account(users, (err, user) => {
         if (err) {
             if (err.errno == 19) {
-                // sqlite error for the duplication of a email adderss.(primery key)
+                // sqlite error for the duplication of a email
                 return res.status(400).send({ "error_message": `Email '${users.email}' already has an account.` });
             }
             else {
